f8733ac4e9f33059f2f14d987e05056d913d1e7c
Varun Rao <vrao_87@Varuns-MacBook-Air.local>
2018-03-06 23:19:20 +0530

    lane follow

--- src/main.cpp
+++ src/main.cpp
@@ -263,8 +263,8 @@ int main() {
                 ptsx.push_back(prev_car_x);
                 ptsx.push_back(car_x);
 
-                ptsx.push_back(prev_car_y);
-                ptsx.push_back(car_y);
+                ptsy.push_back(prev_car_y);
+                ptsy.push_back(car_y);
             }
 
             else
@@ -288,6 +288,16 @@ int main() {
             vector<double> next_wp1 = getXY(car_s + 60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             vector<double> next_wp2 = getXY(car_s +90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            printf("%d ptsx size\n", ptsx.size());
+
             for(int i = 0; i < ptsx.size(); i++)
             {
                 double shift_x = ptsx[i] - ref_x;
@@ -297,6 +307,9 @@ int main() {
                 ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
 
             }
+
+            // printf("%d ptsx size\n", ptsx.size());
+            // printf("%d ptsy size\n", ptsy.size());
 
             // Create a spline 
             tk::spline s;
@@ -319,30 +332,46 @@ int main() {
             double target_y = s(target_x);
             double target_dist = sqrt((target_x * target_x)+(target_y * target_y));
 
-            ptsx.push_back(next_wp0[0]);
-            ptsx.push_back(next_wp1[0]);
-            ptsx.push_back(next_wp2[0]);
 
-            ptsy.push_back(next_wp0[1]);
-            ptsy.push_back(next_wp1[1]);
-            ptsy.push_back(next_wp2[1]);
+            double x_add_on = 0;
+            
+            for(int i  = 1; i <= (50 - previous_path_x.size()); i++)
+            {
+                double N = (target_dist/(0.02 * ref_vel /2.24));
+                double x_point = x_add_on + (target_x/N);
+                double y_point = s(x_point);
 
+                x_add_on = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+                y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+
+            }
 
           	json msgJson;
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 
-            double dist_inc = 0.5;
-            for(int i = 0; i < 50; i++)
-            {
-                double next_s = car_s + (i + 1) * dist_inc;
-                double next_d = 6;
-                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            // double dist_inc = 0.5;
+            // for(int i = 0; i < 50; i++)
+            // {
+            //     double next_s = car_s + (i + 1) * dist_inc;
+            //     double next_d = 6;
+            //     vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
-                next_x_vals.push_back(xy[0]);
-                next_y_vals.push_back(xy[1]);
-            }
+            //     next_x_vals.push_back(xy[0]);
+            //     next_y_vals.push_back(xy[1]);
+            // }
 
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
